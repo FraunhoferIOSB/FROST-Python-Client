4217198aedb4b868ad1cc5dcb6afb53207d87816
tfeseker <feseker@posteo.net>
2023-01-25 10:13:10 +0100

    fixed definitions of relations in entity_types

--- frost_sta_client/model/ext/entity_type.py
+++ frost_sta_client/model/ext/entity_type.py
@@ -19,37 +19,37 @@ EntityTypes = {
         'singular': 'Datastream',
         'plural': 'Datastreams',
         'class': 'frost_sta_client.model.datastream.Datastream',
-        'relations_list': ['Sensor', 'Thing', 'ObservedProperty', 'Observation']
+        'relations_list': ['Sensor', 'Thing', 'ObservedProperty', 'Observations']
     },
     'MultiDatastream': {
         'singular': 'MultiDatastream',
         'plural': 'MultiDatastreams',
         'class': 'frost_sta_client.model.multi_datastream.MultiDatastream',
-        'relations_list': ['Sensor', 'Thing', 'ObservedProperty', 'Observation']
+        'relations_list': ['Sensor', 'Thing', 'ObservedProperties', 'Observations']
     },
     'FeatureOfInterest': {
         'singular': 'FeatureOfInterest',
         'plural': 'FeaturesOfInterest',
         'class': 'frost_sta_client.model.feature_of_interest.FeatureOfInterest',
-        'relations_list': ['Observation']
+        'relations_list': ['Observations']
     },
     'HistoricalLocation': {
         'singular': 'HistoricalLocation',
         'plural': 'HistoricalLocations',
         'class': 'frost_sta_client.model.historical_location.HistoricalLocation',
-        'relations_list': ['Thing', 'Location']
+        'relations_list': ['Thing', 'Locations']
     },
     'Actuator': {
         'singular': 'Actuator',
         'plural': 'Actuators',
         'class': 'frost_sta_client.model.actuator.Actuator',
-        'relations_list': ['TaskingCapability']
+        'relations_list': ['TaskingCapabilities']
     },
     'Location': {
         'singular': 'Location',
         'plural': 'Locations',
         'class': 'frost_sta_client.model.location.Location',
-        'relations_list': ['Thing', 'HistoricalLocation']
+        'relations_list': ['Things', 'HistoricalLocations']
     },
     'Observation': {
         'singular': 'Observation',
@@ -61,19 +61,19 @@ EntityTypes = {
         'singular': 'Thing',
         'plural': 'Things',
         'class': 'frost_sta_client.model.thing.Thing',
-        'relations_list': ['Datastream', 'MultiDatastream', 'Location', 'HistoricalLocation', 'TaskingCapability']
+        'relations_list': ['Datastreams', 'MultiDatastreams', 'Locations', 'HistoricalLocations', 'TaskingCapabilities']
     },
     'ObservedProperty': {
         'singular': 'ObservedProperty',
         'plural': 'ObservedProperties',
         'class': 'frost_sta_client.model.observedproperty.ObservedProperty',
-        'relations_list': ['Datastream', 'MultiDatastream']
+        'relations_list': ['Datastreams', 'MultiDatastreams']
     },
     'Sensor': {
         'singular': 'Sensor',
         'plural': 'Sensors',
         'class': 'frost_sta_client.model.sensor.Sensor',
-        'relations_list': ['Datastream', 'MultiDatastream']
+        'relations_list': ['Datastreams', 'MultiDatastreams']
     },
     'Task': {
         'singular': 'Task',
@@ -85,7 +85,7 @@ EntityTypes = {
         'singular': 'TaskingCapability',
         'plural': 'TaskingCapabilities',
         'class': 'frost_sta_client.model.tasking_capability.TaskingCapability',
-        'relations_list': ['Task', 'Actuator', 'Thing']
+        'relations_list': ['Tasks', 'Actuator', 'Thing']
     },
     'UnitOfMeasurement': {
         'singular': 'UnitOfMeasurement',
