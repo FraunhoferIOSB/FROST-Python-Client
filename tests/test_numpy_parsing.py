d80979ca69837d20ff494e9db2f4582c3f4b8890
Uwe Zeltmann <uwe.zeltmann@iosb.fraunhofer.de>
2025-09-15 08:34:59 +0200

    test: expect numpy data types to be converted in constructors

--- tests/test_numpy_parsing.py
+++ tests/test_numpy_parsing.py
@@ -3,38 +3,46 @@ import pytest
 from frost_sta_client import utils
 from frost_sta_client.model.observation import Observation
 
+def _obs_obj(val):
+    return Observation(id=1, result=val)
 
-def _obs_json(val):
-    return {'@iot.id': 1, 'result': val}
 
-
-def test_parse_numpy_float64_in_result():
-    data = _obs_json(np.float64(0.15))
-    obs = utils.transform_json_to_entity(data, 'frost_sta_client.model.observation.Observation')
+def test_parse_obs_to_dict_numpy_float64_in_result():
+    val = np.float64(0.15)
+    data = _obs_obj(val)
     assert isinstance(obs, Observation)
     assert isinstance(obs.result, float)
     assert obs.result == pytest.approx(0.15)
 
 
-def test_parse_numpy_float32_in_result():
-    data = _obs_json(np.float32(1.25))
-    obs = utils.transform_json_to_entity(data, 'frost_sta_client.model.observation.Observation')
+def test_parse_obs_to_dict_numpy_float32_in_result():
+    val = np.float32(1.25)
+    data = _obs_obj(val)
+    assert isinstance(obs, Observation)
     assert isinstance(obs.result, float)
     assert obs.result == pytest.approx(1.25)
 
 
-def test_parse_numpy_int_in_result():
-    data = _obs_json(np.int64(5))
-    obs = utils.transform_json_to_entity(data, 'frost_sta_client.model.observation.Observation')
-    # accepted and converted to Python-int
+def test_parse_obs_to_dict_numpy_int_in_result():
+    val = np.int64(5)
+    data = _obs_obj(val)
+    assert isinstance(obs, Observation)
     assert isinstance(obs.result, int)
     assert obs.result == 5
 
 
-def test_parse_numpy_float_inside_nested_result_dict():
-    data = _obs_json({'v': np.float64(2.5)})
-    obs = utils.transform_json_to_entity(data, 'frost_sta_client.model.observation.Observation')
-    # nested numpy.float shall be accepted and converted to float
+def test_parse_obs_to_dict_numpy_float_dict_float64_in_result():
+    val = {'v': np.float64(2.5)}
+    data = _obs_obj(val)
+    assert isinstance(obs, Observation)
     assert isinstance(obs.result, dict)
     assert isinstance(obs.result['v'], float)
     assert obs.result['v'] == pytest.approx(2.5)
+
+
+def test_parse_obs_to_dict_numpy_array_float64_in_result():
+    val = np.array([np.float64(0.15), np.float64(-1.457e5)])
+    data = _obs_obj(val)
+    assert isinstance(obs, Observation)
+    assert isinstance(obs.result, list)
+    assert obs.result == pytest.approx([0.15, -1.457e5])
